9fb1cbfaffebf0ae5f1910b54e0c778bcbce0b69
Brian Solano <brian_solano110012@yahoo.com>
2025-03-10 22:25:15 -0500

    revised mutations js and type defs js files for apollo

--- server/src/schema/typeDefs.js
+++ server/src/schema/typeDefs.js
@@ -9,12 +9,19 @@ const typeDefs = gql`
     headline: String
   }
 
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
   type Query {
     users: [User]
     user(id: ID!): User
   }
 
   type Mutation {
+    loginUser(email: String!, password: String!): AuthPayload
+    registerUser(username: String!, email: String!, password: String!): User
     updateProfile(id: ID!, username: String, headline: String): User
   }
 `;
