28554fa9b9b3301eb8b0167f303a0498cb3101af
Ethan Maller <mallerethan4@gmail.com>
2025-03-11 19:53:19 -0500

    Fixing cors issue in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -20,8 +20,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 
 app.use(express.json()); 
-app.use(cors({ origin: '*', credentials: true }));
-// ['https://dev-connect-1-eiz8.onrender.com','http://localhost:5173', 'http://127.0.0.1:5173']
+app.use(cors({ origin: ['https://dev-connect-1-eiz8.onrender.com','http://localhost:5173', 'http://127.0.0.1:5173'], credentials: true }));
 
 // Initialize Apollo Server
 const server = new ApolloServer({
